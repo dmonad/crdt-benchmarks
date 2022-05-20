8a10c4679356dc961887c728593da90ab69f8175
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2022-05-20 13:49:43 +0200

    added ywasm package benchmarks

---
+++ benchmarks/ywasm/factory.js
@@ -0,0 +1,138 @@
+
+import { AbstractCrdt, CrdtFactory } from '../../js-lib/index.js' // eslint-disable-line
+import * as Y from 'ywasm'
+
+export const name = 'ywasm'
+
+/**
+ * @implements {CrdtFactory}
+ */
+export class YwasmFactory {
+  /**
+   * @param {function(Uint8Array):void} [updateHandler]
+   */
+  create (updateHandler) {
+    return new YwasmCRDT(updateHandler)
+  }
+
+  getName () {
+    return name
+  }
+}
+
+/**
+ * @implements {AbstractCrdt}
+ */
+export class YwasmCRDT {
+  /**
+   * @param {function(Uint8Array):void} [updateHandler]
+   */
+  constructor (updateHandler) {
+    this.ydoc = new Y.YDoc()
+    if (updateHandler) {
+      this.ydoc.onUpdate(update => {
+        updateHandler(update)
+      })
+    }
+    this.yarray = this.ydoc.getArray('array')
+    this.ymap = this.ydoc.getMap('map')
+    this.ytext = this.ydoc.getText('text')
+    this.txn = null
+  }
+
+  /**
+   * @return {Uint8Array|string}
+   */
+  getEncodedState () {
+    return Y.encodeStateAsUpdateV2(this.ydoc)
+  }
+
+  /**
+   * @param {Uint8Array} update
+   */
+  applyUpdate (update) {
+    Y.applyUpdateV2(this.ydoc, update)
+  }
+
+  /**
+   * Insert several items into the internal shared array implementation.
+   *
+   * @param {number} index
+   * @param {Array<any>} elems
+   */
+  insertArray (index, elems) {
+    this.yarray.insert(this.txn, index, elems)
+  }
+
+  /**
+   * Delete several items into the internal shared array implementation.
+   *
+   * @param {number} index
+   * @param {number} len
+   */
+  deleteArray (index, len) {
+    this.yarray.delete(this.txn, index, len)
+  }
+
+  /**
+   * @return {Array<any>}
+   */
+  getArray () {
+    return this.yarray.toJson()
+  }
+
+  /**
+   * Insert text into the internal shared text implementation.
+   *
+   * @param {number} index
+   * @param {string} text
+   */
+  insertText (index, text) {
+    this.ytext.insert(this.txn, index, text, null)
+  }
+
+  /**
+   * Delete text from the internal shared text implementation.
+   *
+   * @param {number} index
+   * @param {number} len
+   */
+  deleteText (index, len) {
+    this.ytext.delete(this.txn, index, len)
+  }
+
+  /**
+   * @return {string}
+   */
+  getText () {
+    return this.ytext.toString()
+  }
+
+  /**
+   * @param {function (AbstractCrdt): void} f
+   */
+  transact (f) {
+    this.txn = this.txn || this.ydoc.beginTransaction()
+    try {
+      f(this)
+    } finally {
+      this.txn.free()
+      this.txn = null
+    }
+  }
+
+  /**
+   * @param {string} key
+   * @param {any} val
+   */
+  setMap (key, val) {
+    this.ymap.set(this.txn, key, val)
+  }
+
+  /**
+   * @return {Map<string,any> | Object<string, any>}
+   */
+  getMap () {
+    return this.ymap.toJson()
+  }
+}
