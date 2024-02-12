796f70250c8c003dfb3a50369d0b2733a760e54d
Kevin Jahns <kevin.jahns@protonmail.com>
2024-02-12 20:08:23 +0100

    fix automerge measure bundle script

--- benchmarks/ywasm/run.js
+++ benchmarks/ywasm/run.js
@@ -2,6 +2,6 @@ import { YwasmFactory } from './factory.js'
 import { runBenchmarks, writeBenchmarkResultsToFile } from '../../js-lib/index.js'
 
 ;(async () => {
-  await runBenchmarks(new YwasmFactory(), testName => true) // !testName.startsWith('[B4'))
+  await runBenchmarks(new YwasmFactory(), testName => !testName.startsWith('[B4x100')) // !testName.startsWith('[B4'))
   writeBenchmarkResultsToFile('../results.json', testName => true)
 })()
